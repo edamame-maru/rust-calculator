fa796bd8aa832ae45e43a56bc2d265151d211462
edamame-maru <so1maru.ueda@gmail.com>
2024-05-30 21:39:33 +0100

    worked on it a bit

--- rust-calculator/src/main.rs
+++ rust-calculator/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::cmp::Ordering;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Enter number of operands: ");
+    io::stdout().flush().unwrap();
+
+    let mut operands = String::new();
+
+    io::stdin()
+        .read_line(&mut operands)
+        .expect("Failed to read line");
+
+    let mut operands: i32 = operands.trim().parse().expect("Failed to parse to integer.");
+    
+    let operators: i32 = operands - 2; 
+
+    println!("{} operands, therefore {} operators.", &mut operands, operators);
 }
