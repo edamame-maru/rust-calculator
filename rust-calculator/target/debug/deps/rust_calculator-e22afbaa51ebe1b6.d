dfcff6664015c6842998eb4a7ab76ed9a350783b
edamame-maru <so1maru.ueda@gmail.com>
2024-05-30 20:52:49 +0100

    added helloworld

---
+++ rust-calculator/target/debug/deps/rust_calculator-e22afbaa51ebe1b6.d
@@ -0,0 +1,5 @@
+/home/edamame-maru/Desktop/rust-calculator/rust-calculator/target/debug/deps/rust_calculator-e22afbaa51ebe1b6: src/main.rs
+
+/home/edamame-maru/Desktop/rust-calculator/rust-calculator/target/debug/deps/rust_calculator-e22afbaa51ebe1b6.d: src/main.rs
+
+src/main.rs:
---
+++ rust-calculator/target/debug/rust-calculator.d
@@ -0,0 +1,1 @@
+/home/edamame-maru/Desktop/rust-calculator/rust-calculator/target/debug/rust-calculator: /home/edamame-maru/Desktop/rust-calculator/rust-calculator/src/main.rs
